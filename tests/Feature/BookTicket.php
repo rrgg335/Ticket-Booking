91ebad7386dcc426173cf1902e26c2feb513d87f
Rohan <rohan@mind-roots.com>
2023-02-17 15:52:16 +0530

    Initial Upload

---
+++ tests/Feature/BookTicket.php
@@ -0,0 +1,10 @@
+<?php
+namespace Tests\Feature;
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+class BookTicket extends TestCase{
+    public function test_the_application_returns_a_successful_response(): void{
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+}
